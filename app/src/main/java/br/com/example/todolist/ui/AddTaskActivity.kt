757b135be86f18cc4b920f35bf39250af12a537b
Emmanuel-Costa <emmanuelcosta@id.uff.br>
2021-08-13 11:16:15 -0300

    adding viewbinding

---
+++ app/src/main/java/br/com/example/todolist/ui/AddTaskActivity.kt
@@ -0,0 +1,12 @@
+package br.com.example.todolist.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
